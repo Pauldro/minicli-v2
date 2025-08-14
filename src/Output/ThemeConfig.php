0c1eae7fb2e0f2e8ee5df23e6e67bf4f410fbe8e
Paul Gomez <pauldro952@outlook.com>
2025-08-14 09:00:14 -0500

    Create ThemeConfig.php

---
+++ src/Output/ThemeConfig.php
@@ -0,0 +1,58 @@
+<?php namespace Pauldro\Minicli\v2\Output;
+// Minicli
+use Minicli\Output\ThemeStyle;
+
+final class ThemeConfig
+{
+    public function __construct(
+        public ThemeStyle $default,
+        public ThemeStyle $alt,
+        public ThemeStyle $error,
+        public ThemeStyle $error_alt,
+        public ThemeStyle $success,
+        public ThemeStyle $success_alt,
+        public ThemeStyle $info,
+        public ThemeStyle $info_alt,
+        public ThemeStyle $info_header,
+        public ThemeStyle $bold,
+        public ThemeStyle $dim,
+        public ThemeStyle $italic,
+        public ThemeStyle $underline,
+        public ThemeStyle $invert
+    ) {
+    }
+
+    public static function make(
+        ThemeStyle $default,
+        ThemeStyle $alt,
+        ThemeStyle $error,
+        ThemeStyle $error_alt,
+        ThemeStyle $success,
+        ThemeStyle $success_alt,
+        ThemeStyle $info,
+        ThemeStyle $info_alt,
+        ThemeStyle $info_header,
+        ThemeStyle $bold,
+        ThemeStyle $dim,
+        ThemeStyle $italic,
+        ThemeStyle $underline,
+        ThemeStyle $invert
+    ): self {
+        return new self(
+            $default,
+            $alt,
+            $error,
+            $error_alt,
+            $success,
+            $success_alt,
+            $info,
+            $info_alt,
+            $info_header,
+            $bold,
+            $dim,
+            $italic,
+            $underline,
+            $invert
+        );
+    }
+}
