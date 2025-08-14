e067f491132f3aa1ade9d1b7183ba60e2fa8a3b7
Paul Gomez <pauldro952@outlook.com>
2025-08-14 01:51:20 -0500

    Create CliTheme.php

---
+++ src/Output/Theme/CliTheme.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pauldro\Minicli\v2\Output\Theme;
+
+use Minicli\Output\CLIColors;
+
+class CliTheme extends DefaultTheme
+{
+    /**
+     * get the colors
+     *
+     * @return array<string, array<int, string>>
+     */
+    public function getThemeColors(): array
+    {
+        return [
+            'default'     => [CLIColors::$FG_WHITE],
+            'alt'         => [CLIColors::$FG_BLACK, CLIColors::$BG_WHITE],
+            'error'       => [CLIColors::$FG_RED],
+            'error_alt'   => [CLIColors::$FG_WHITE, CLIColors::$BG_RED],
+            'success'     => [CLIColors::$FG_GREEN],
+            'success_alt' => [CLIColors::$FG_WHITE, CLIColors::$BG_GREEN],
+            'info'        => [CLIColors::$FG_CYAN],
+            'info_alt'    => [CLIColors::$FG_WHITE, CLIColors::$BG_CYAN],
+            'info_header' => [CLIColors::$FG_BLUE],
+            'bold'        => [CliColors::$BOLD],
+            'dim'         => [CliColors::$DIM],
+            'italic'      => [CliColors::$ITALIC],
+            'underline'   => [CliColors::$UNDERLINE],
+            'invert'      => [CliColors::$INVERT]
+        ];
+    }
+}
