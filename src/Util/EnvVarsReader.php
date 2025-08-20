af643c3f5e8454d507e7cf942e84250a4640098c
Paul Gomez <pauldro952@outlook.com>
2025-08-20 12:18:00 -0500

    Create EnvVarsReader.php

---
+++ src/Util/EnvVarsReader.php
@@ -0,0 +1,27 @@
+<?php namespace Pauldro\Minicli\v2\Util;
+
+/**
+ * EnvVarsReader
+ * 
+ * Utility for reading values from $_ENV
+ */
+class EnvVarsReader {
+    public function exists(string $key) : bool
+    {
+        return array_key_exists($key, $_ENV);
+    }
+
+    public function get(string $key, $default = '') : string
+    {
+        if ($this->exists($key) === false) {
+            return $default;
+        }
+        return $_ENV[$key];
+    }
+
+    public function getBool(string $key) : bool
+    {
+        $value = $this->get($key, 'false');
+        return $value == 'true';
+    }
+}
